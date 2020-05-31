5d1f2a214465710f44bc49c67ab3d692dd3bde81
dep-vol <alex.tecto.msk@gmail.com>
2020-05-31 06:35:11 +0300

    Updates

---
+++ precache-manifest.58bd8a7405f1313e4638cc81259efffc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2477aaa9d3a99ab842a2695af6d578cb",
+    "url": "/Shop_practice_MobX/index.html"
+  },
+  {
+    "revision": "47006f1cbad89718bf6e",
+    "url": "/Shop_practice_MobX/static/css/main.0352b9fd.chunk.css"
+  },
+  {
+    "revision": "2910be0ba4b2ae4c5ae0",
+    "url": "/Shop_practice_MobX/static/js/2.6d0cff1b.chunk.js"
+  },
+  {
+    "revision": "3d377f571d4c27f071860c1026c7a1b2",
+    "url": "/Shop_practice_MobX/static/js/2.6d0cff1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47006f1cbad89718bf6e",
+    "url": "/Shop_practice_MobX/static/js/main.af121a23.chunk.js"
+  },
+  {
+    "revision": "6a3758db928eaae5013d",
+    "url": "/Shop_practice_MobX/static/js/runtime-main.02dfbe9f.js"
+  },
+  {
+    "revision": "6b1a547c1fafd06aa25952b49b408f74",
+    "url": "/Shop_practice_MobX/static/media/cart.6b1a547c.jpg"
+  },
+  {
+    "revision": "cea54723e73945d7062458086a12bf6e",
+    "url": "/Shop_practice_MobX/static/media/iphone_PNG5733.cea54723.png"
+  }
+]);
